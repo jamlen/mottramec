1c2fc28373a339dae982b41426bfddb208ac74c2
James Allen <code@james-allen.co.uk>
2015-05-07 22:39:02 +0100

    Adding extra fields to api output

--- routes/api/sermon.js
+++ routes/api/sermon.js
@@ -10,6 +10,7 @@ exports = module.exports = function(req, res) {
     locals.filters = {
         sermon: req.params.sermon,
         latest: (req.params.sermon === 'latest'),
+        limit: req.params.limit || 10,
         old: !_.isNaN(_.parseInt(req.params.sermon))
     };
 
@@ -20,6 +21,8 @@ exports = module.exports = function(req, res) {
             series: _.isEmpty(sermon.series) ? null : sermon.series.name,
             speaker: sermon.speaker.name.full,
             thumbnail: sermon.thumbnail.img,
+            bibleRefs: sermon.bibleRefs,
+            audio: sermon.audio.url,
             url: 'http://' + host + '/sermon/'+sermon.slug
         };
         callback(apiSermon);
@@ -27,11 +30,10 @@ exports = module.exports = function(req, res) {
 
     var q = Sermon.model.findOne()
         .where('state', 'published')
-        .limit(1)
         .populate('series speaker bibleRefs');
 
     if (locals.filters.latest) {
-        q = q.sort({date:-1});
+        q = q.sort({date:-1}).limit(1);
     } else if (locals.filters.old) {
         q = q.where('oldId', locals.filters.sermon);
     } else {
